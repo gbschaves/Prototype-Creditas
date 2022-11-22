0f2f12c328b42ac3001f758dcf70c5a1d0f0997d
gbschaves <gb.chaves@hotmail.com>
2022-11-22 15:38:14 -0300

    Finalizado

--- assets/JavaScript/view/callSpinner.js
+++ assets/JavaScript/view/callSpinner.js
@@ -1,5 +1,9 @@
 function callSpinner() {
     
     let spinner = document.getElementById('spinner-hide');
-    spinner.classList.remove('hide');
+    spinner.classList.remove('hide')
+
+    setTimeout(() => {
+        spinner.classList.add('hide')
+    }, 3000);
 }
