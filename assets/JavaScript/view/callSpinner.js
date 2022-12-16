eac99dd2863005091eb4892182e3fae7e9dbd628
Guilherme Chaves <53453346+gbschaves@users.noreply.github.com>
2022-12-16 16:21:55 -0300

    Delete assets directory

--- assets/JavaScript/view/callSpinner.js
+++
@@ -1,9 +0,0 @@
-function callSpinner() {
-    
-    let spinner = document.getElementById('spinner-hide');
-    spinner.classList.remove('hide')
-
-    setTimeout(() => {
-        spinner.classList.add('hide')
-    }, 3000);
-}
--- assets/JavaScript/view/erroView.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function () {
-    let erroAPI = localStorage.getItem('errors')
-    console.log(erroAPI)
-    let erroScreen = document.getElementById('content__text')
-    erroScreen.innerHTML += `
-    <h1 class="error__message">${JSON.parse(erroAPI)}<br> 
-    Tente novamente.</h1>
-    `
-  })
